a427b1106160b140e3d192bd487c89c4c1e18a0d
SakuKosonen <saku.kosonen96@gmail.com>
2020-02-07 23:51:53 +0200

    mikko hoitaa

---
+++ build/precache-manifest.6b36458fdf3fd8ed2095f55aa7d50d35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb920862f296992ec7dabfc416bd1d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c8da391e987ba0efa15",
+    "url": "/static/css/main.d9603cdc.chunk.css"
+  },
+  {
+    "revision": "a4ff7f15b0a9c86bc646",
+    "url": "/static/js/2.b60c7687.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b60c7687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8da391e987ba0efa15",
+    "url": "/static/js/main.263a6eea.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
